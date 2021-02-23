d667053838350f7a09e3e79a6e8e2ac36ec9da78
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-02-23 03:50:34 +0200

    registrationand aunthentication with API

--- app/src/main/java/com/example/istudy/LoginActivity.java
+++ app/src/main/java/com/example/istudy/LoginActivity.java
@@ -6,10 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.istudy.models.LoginRequest;
+import com.example.istudy.models.LoginResponse;
+import com.example.istudy.services.ApiClient;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +26,9 @@ public class LoginActivity extends AppCompatActivity {
     Button login;
     @BindView(R.id.toRegisterPage)
     Button toRegisterPage;
+
+    @BindView(R.id.username) EditText name;
+    @BindView(R.id.password) EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +39,11 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-                startActivity(intent);
+                String Name = name.getText().toString();
+                String Password = password.getText().toString();
+
+                LoginRequest loginRequest = new LoginRequest(Name,Password);
+                loginUser(loginRequest);
             }
         });
         toRegisterPage.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +51,36 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+    }
+    public void loginUser(LoginRequest loginRequest){
+        Call<LoginResponse> loginResponseCall = ApiClient.getService().LoginUser(loginRequest);
+        loginResponseCall.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if(response.isSuccessful()){
+                    String message = "Succeffully logged in";
+                    Toast.makeText(LoginActivity.this,message,Toast.LENGTH_SHORT).show();
+                    LoginResponse loginResponse = response.body();
+                    startActivity(new Intent(LoginActivity.this,HomeActivity.class).putExtra("data",loginResponse));
+                    finish();
+
+
+
+                }else{
+                    String message = "An error occured. Please try again";
+                    Toast.makeText(LoginActivity.this,message,Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                String message = t.getLocalizedMessage();
+                Toast.makeText(LoginActivity.this,message,Toast.LENGTH_SHORT).show();
+
             }
         });
 
--- app/src/main/java/com/example/istudy/RegisterActivity.java
+++ app/src/main/java/com/example/istudy/RegisterActivity.java
@@ -6,9 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.istudy.models.RegisterRequest;
+import com.example.istudy.models.RegisterResponse;
+import com.example.istudy.services.ApiClient;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +25,12 @@ public class RegisterActivity extends AppCompatActivity {
     Button register;
     @BindView(R.id.toLoginPage)
     Button toLoginPage;
+
+    @BindView(R.id.email) EditText email;
+    @BindView(R.id.username) EditText name;
+
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.confirmPassword) EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +41,15 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this,HomeActivity.class);
-                startActivity(intent);
+                String Name = name.getText().toString();
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+                String Password2 = confirmPassword.getText().toString();
+
+                RegisterRequest registerRequest = new RegisterRequest( Name, Password, Password2, Email);
+
+
+                registerUser(registerRequest);
             }
         });
         toLoginPage.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +59,34 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void registerUser(RegisterRequest registerRequest) {
+        Call<RegisterResponse> registerResponseCall= ApiClient.getService().RegisterUser(registerRequest);
+        registerResponseCall.enqueue(new Callback<RegisterResponse>() {
+            @Override
+            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+                if(response.isSuccessful()){
+                    String message = "Successfully logged in";
+                    Toast.makeText(RegisterActivity.this,message,Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }else {
+                    String message = "An error occured. Please try again";
+                    Toast.makeText(RegisterActivity.this,message,Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<RegisterResponse> call, Throwable t) {
+                String message = t.getLocalizedMessage();
+                Toast.makeText(RegisterActivity.this,message,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
