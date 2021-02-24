4c7fd2c7db8c3b281a53d983da455a7954160320
Benitha250 <benithauwase05@gmail.com>
2021-02-24 07:48:36 +0200

    correcting grammar errors

--- app/src/main/java/com/example/istudy/HomeActivity.java
+++ app/src/main/java/com/example/istudy/HomeActivity.java
@@ -3,13 +3,18 @@ package com.example.istudy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class HomeActivity extends AppCompatActivity {
 
